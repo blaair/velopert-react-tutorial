b7769c2f1f4635d8ffdd7aa3d0d511941965824f
jiyoung <xoxopjy@gmail.com>
2022-05-16 17:10:09 +0900

    commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # velopert-react-tutorial
+
 벨로퍼트와 함께하는 모던 리액트
 (https://react.vlpt.us/)
---
+++ mashup-todolist/src/components/TodoHead.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoHeadBlock = styled.div`
+`
+
+function TodoHead() {
+    return (
+        <TodoHeadBlock>
+            <h1>2022년 1월 1일</h1>
+        </TodoHeadBlock>   
+    )
+}
+
+export default TodoHead;
